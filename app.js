dd0205fe194b2ca338f16a0a39b38081ffb132e3
sholayman99 <mdsholayman998@gmail.com>
2023-09-02 00:00:38 +0600

    app structure is completed

---
+++ app.js
@@ -0,0 +1,5 @@
+/**
+ * Author: Md Sholayman
+ * Description: This is the file where config has holded.
+ * Date : 01 September,2023.
+ */
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Author: Md Sholayman
+ * Description: This is the file where application is running.
+ * Date : 01 September,2023.
+ */
+
+const express = require("express");
